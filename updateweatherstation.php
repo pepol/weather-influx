2c80420a131a48cc99bbb3e83dda714f87cd8b9e
Peter Polacik <peter@polacik.org>
2021-01-05 16:53:52 +0100

    Add station ID
    Signed-off-by: Peter Polacik <peter@polacik.org>

--- updateweatherstation.php
+++ updateweatherstation.php
@@ -3,39 +3,40 @@
 include('config.php');
 
 if (!defined('DB_URL')) {
-	define('DB_URL', 'http://localhost:8086');
+  define('DB_URL', 'http://localhost:8086');
 }
 
 if (!defined('DB_NAME')) {
-	define('DB_NAME', 'meteo');
+  define('DB_NAME', 'meteo');
 }
 
 if (!defined('DB_SERIES')) {
-	define('DB_SERIES', 'weather');
+  define('DB_SERIES', 'weather');
 }
 
 $content = json_encode($_GET);
+$stationID = $_SERVER["X_ARCA_STATION_ID"] ?: "XUNKNOWN"
 
 # Save data for verification
 file_put_contents('latest.json', $content);
 
 function storeData($content, $dbURL, $dbName, $dbSeries) {
-	$opts = [
-		'http' => [
-			'method' => "POST",
-			'content' => $dbSeries . " " . http_build_query($content, '', ','),
-		]
-	];
+  $opts = [
+    'http' => [
+      'method' => "POST",
+      'content' => $dbSeries . ",id=" . $stationID . " " . http_build_query($content, '', ','),
+    ]
+  ];
 
-	$params = [
-		"db" => $dbName
-	];
+  $params = [
+    "db" => $dbName
+  ];
 
-	file_get_contents(
-		$dbURL . '/write?' . http_build_query($params),
-		false,
-		stream_context_create($opts)
-	);
+  file_get_contents(
+    $dbURL . '/write?' . http_build_query($params),
+    false,
+    stream_context_create($opts)
+  );
 }
 
 storeData($content, DB_URL, DB_NAME, DB_SERIES);
